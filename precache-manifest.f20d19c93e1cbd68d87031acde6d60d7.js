0f3fd415f98312b3f5396fd3e049e63cf14140ca
Stimoe <stimoe3@gmail.com>
2019-10-25 16:08:39 -0700

    Updates

---
+++ precache-manifest.f20d19c93e1cbd68d87031acde6d60d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "970993defcf058b0001269f11912d2d9",
+    "url": "/clicker3/index.html"
+  },
+  {
+    "revision": "388f5329ebcbcb8a1f9e",
+    "url": "/clicker3/static/css/main.69c27f1b.chunk.css"
+  },
+  {
+    "revision": "043bb9c976eea671b5b0",
+    "url": "/clicker3/static/js/2.36fe18c6.chunk.js"
+  },
+  {
+    "revision": "388f5329ebcbcb8a1f9e",
+    "url": "/clicker3/static/js/main.8331a706.chunk.js"
+  },
+  {
+    "revision": "3a0fedad0c12ec5e1f6c",
+    "url": "/clicker3/static/js/runtime-main.271f5706.js"
+  }
+]);
